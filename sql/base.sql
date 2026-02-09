2979071deccde194292b23782568c20932ac7568
ChiuTien <chiumandresychristian@gmail.com>
2026-02-09 15:52:34 +0300

    finition base

--- sql/base.sql
+++ sql/base.sql
@@ -10,3 +10,26 @@ CREATE OR REPLACE TABLE statut(
     idStatut INT PRIMARY KEY AUTO_INCREMENT,
     nomStatut VARCHAR(100)
 );
+CREATE OR REPLACE TABLE categorie(
+    idCategorie INT PRIMARY KEY AUTO_INCREMENT,
+    nomCategorie VARCHAR(100)
+);
+CREATE OR REPLACE TABLE image(
+    idImage INT PRIMARY KEY AUTO_INCREMENT,
+    idObjet INT,
+    urlImage VARCHAR(255)
+);
+CREATE OR REPLACE TABLE objet(
+    idObjet INT PRIMARY KEY AUTO_INCREMENT,
+    nomObjet VARCHAR(100),
+    idCategorie INT,
+    idProprietaire INT,
+    descriptionObjet TEXT
+);
+CREATE OR REPLACE TABLE echange(
+    idEchange INT PRIMARY KEY AUTO_INCREMENT,
+    idObjetOffert INT,
+    idObjetDemande INT,
+    idStatut INT,
+    dateEchange DATE
+);
