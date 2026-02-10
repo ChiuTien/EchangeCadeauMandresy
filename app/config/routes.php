4c0e3386fd91f049178769b2de6898d96f535fa6
ChiuTien <chiumandresychristian@gmail.com>
2026-02-10 10:11:45 +0300

    Ajout route pour aller dans inscription

--- app/config/routes.php
+++ app/config/routes.php
@@ -16,6 +16,9 @@ $router->group('', function(Router $router) use ($app) {
 	$router->get('/', function() use ($app) {
 		$app->render('login');
 	});
+	$router->get('/inscription',function() use ($app) {
+		$app->render('inscription');
+	});
 
 	$router->get('/hello-world/@name', function($name) {
 		echo '<h1>Hello world! Oh hey '.$name.'!</h1>';
